e952dd8f78f852a5c53ca0344108044ba84f6596
Bram Dingelstad <bram@oneartplea.se>
2020-03-16 09:53:50 +0100

    Fixed a small mistake in the install file

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ sudo apt install -qq -y libnginx-mod-http-lua lua-posix
 sudo mkdir -p /usr/lib/neh
 sudo curl -sL \
     https://raw.githubusercontent.com/oap-bram/neh/master/neh.lua \
-    -o /usr/lib/neh.lua
+    -o /usr/lib/neh/neh.lua
